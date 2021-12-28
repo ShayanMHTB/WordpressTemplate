8f28291914ed85231dbfe0c2534ab815cfe88eb6
Shayan Mahtabi <shayan.mhtb@gmail.com>
2021-12-28 10:05:45 +0100

    docker images updated

--- images/mysql/entrypoint.sh
+++ images/mysql/entrypoint.sh
@@ -1,112 +1,59 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# MySQL Container Entrypoint
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ─────────────────────────────────────────────────────────
+#  MariaDB first-run bootstrap: users, DB, and permissions
+#  All values come from environment variables (.env)
+# ─────────────────────────────────────────────────────────
 
-set -e
+: "${DB_ROOT_PASSWORD:?Missing DB_ROOT_PASSWORD}"
+: "${DB_NAME:?Missing DB_NAME}"
+: "${DB_USER:?Missing DB_USER}"
+: "${DB_PASSWORD:?Missing DB_PASSWORD}"
 
-log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] MySQL: $*"
-}
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
-# Initialize MySQL data directory if empty
-initialize_database() {
-    if [ ! -d "/var/lib/mysql/mysql" ]; then
-        log "Initializing MySQL database"
-        
-        # Install MySQL system tables
-        mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm
-        
-        log "MySQL database initialized"
-    fi
-}
+# Initialize datadir if empty (MariaDB 10.11+)
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "✨ Initializing MariaDB data directory…"
+  mariadb-install-db \
+    --user=mysql \
+    --datadir=/var/lib/mysql \
+    --skip-test-db \
+    --auth-root-authentication-method=normal
+fi
 
-# Start MySQL temporarily for setup
-start_temp_mysql() {
-    log "Starting temporary MySQL server"
-    mysqld --user=mysql --skip-networking --socket=/tmp/mysql_temp.sock &
-    local temp_pid=$!
-    
-    # Wait for MySQL to start
-    local count=0
-    until mysql --socket=/tmp/mysql_temp.sock -e "SELECT 1" >/dev/null 2>&1; do
-        count=$((count + 1))
-        if [ $count -gt 30 ]; then
-            log "Temporary MySQL startup timeout!"
-            exit 1
-        fi
-        sleep 1
-    done
-    
-    log "Temporary MySQL server started"
-    echo $temp_pid
-}
+# Start temporary server (no networking)
+echo "🚀 Starting MariaDB (bootstrap)…"
+mariadbd --skip-networking --socket=/run/mysqld/mysqld.sock --user=mysql &
+BOOT_PID=$!
 
-# Setup MySQL users and databases
-setup_mysql() {
-    local temp_pid=$1
-    
-    log "Setting up MySQL users and databases"
-    
-    # Set root password
-    if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
-        mysql --socket=/tmp/mysql_temp.sock -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-        mysql --socket=/tmp/mysql_temp.sock -e "CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-        mysql --socket=/tmp/mysql_temp.sock -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-        log "Root password set"
-    fi
-    
-    # Create application database and user
-    if [ -n "$MYSQL_DATABASE" ]; then
-        mysql --socket=/tmp/mysql_temp.sock -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-        log "Database '$MYSQL_DATABASE' created"
-    fi
-    
-    if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ]; then
-        mysql --socket=/tmp/mysql_temp.sock -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-        
-        if [ -n "$MYSQL_DATABASE" ]; then
-            mysql --socket=/tmp/mysql_temp.sock -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';"
-        fi
-        
-        log "User '$MYSQL_USER' created"
-    fi
-    
-    mysql --socket=/tmp/mysql_temp.sock -e "FLUSH PRIVILEGES;"
-    
-    # Stop temporary MySQL
-    log "Stopping temporary MySQL server"
-    kill $temp_pid
-    wait $temp_pid 2>/dev/null || true
-}
+# Wait for socket
+for i in {1..60}; do
+  if mariadb-admin --socket=/run/mysqld/mysqld.sock ping &>/dev/null; then
+    break
+  fi
+  sleep 1
+done
 
-main() {
-    log "Starting MySQL container initialization"
-    
-    # Initialize database if needed
-    initialize_database
-    
-    # Check if this is the first run
-    if [ ! -f "/var/lib/mysql/.mysql_configured" ]; then
-        # Start temporary MySQL for configuration
-        temp_pid=$(start_temp_mysql)
-        
-        # Setup users and databases
-        setup_mysql $temp_pid
-        
-        # Mark as configured
-        touch /var/lib/mysql/.mysql_configured
-        
-        log "MySQL configuration complete"
-    else
-        log "MySQL already configured, skipping setup"
-    fi
-    
-    log "Starting MySQL server"
-    
-    # Execute the main command
-    exec "$@" --user=mysql
-}
+# Secure and create DB/user
+echo "🔐 Securing root account and creating database/user…"
+mariadb --protocol=socket --socket=/run/mysqld/mysqld.sock <<-SQL
+  ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+  DELETE FROM mysql.user WHERE User='' OR (User='root' AND Host NOT IN ('localhost'));
+  FLUSH PRIVILEGES;
 
-main "$@"
+  CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+  CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+  GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+  FLUSH PRIVILEGES;
+SQL
+
+# Stop bootstrap server
+mariadb-admin --socket=/run/mysqld/mysqld.sock -uroot -p"${DB_ROOT_PASSWORD}" shutdown
+
+echo "✅ MariaDB initialized."
+
+# Exec real server (takes over PID 1 via tini)
+exec "$@"
--- images/wordpress/entrypoint.sh
+++ images/wordpress/entrypoint.sh
@@ -1,76 +1,90 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# WordPress Container Entrypoint
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ───────────────────────────────────────────────────────────────
+#  WordPress first-run:
+#   • Download core if volume empty
+#   • Generate wp-config.php from env
+#   • Install site via wp-cli (optional)
+#   • Then start Apache
+# ───────────────────────────────────────────────────────────────
 
-set -e
+# Required DB vars
+: "${DB_HOST:?Missing DB_HOST}"
+: "${DB_PORT:?Missing DB_PORT}"
+: "${DB_NAME:?Missing DB_NAME}"
+: "${DB_USER:?Missing DB_USER}"
+: "${DB_PASSWORD:?Missing DB_PASSWORD}"
 
-log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] WordPress: $*"
-}
+# Optional WP vars
+WP_URL="${WP_URL:-http://localhost:8000}"
+WP_TITLE="${WP_TITLE:-My Local WP}"
+WP_ADMIN_USER="${WP_ADMIN_USER:-admin}"
+WP_ADMIN_PASSWORD="${WP_ADMIN_PASSWORD:-admin}"
+WP_ADMIN_EMAIL="${WP_ADMIN_EMAIL:-admin@example.com}"
+WP_TABLE_PREFIX="${WP_TABLE_PREFIX:-wp_}"
+WP_DEBUG="${WP_DEBUG:-true}"
 
-# Wait for MySQL to be ready
-wait_for_mysql() {
-    log "Waiting for MySQL connection..."
-    
-    local count=0
-    until mysql -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-        count=$((count + 1))
-        if [ $count -gt 30 ]; then
-            log "MySQL connection timeout!"
-            exit 1
-        fi
-        log "MySQL not ready, waiting... ($count/30)"
-        sleep 2
-    done
-    
-    log "MySQL connection established"
-}
+chown -R www-data:www-data /var/www/html || true
 
-# Download and setup WordPress if not exists
-setup_wordpress() {
-    if [ ! -f index.php ]; then
-        log "WordPress not found, downloading version: $WORDPRESS_VERSION"
-        
-        if [ "$WORDPRESS_VERSION" = "latest" ]; then
-            wp core download --allow-root
-        else
-            wp core download --version="$WORDPRESS_VERSION" --allow-root
-        fi
-        
-        log "WordPress downloaded successfully"
-    fi
-    
-    # Create wp-config.php if it doesn't exist
-    if [ ! -f wp-config.php ]; then
-        log "Creating wp-config.php"
-        
-        wp config create \
-            --dbname="$WORDPRESS_DB_NAME" \
-            --dbuser="$WORDPRESS_DB_USER" \
-            --dbpass="$WORDPRESS_DB_PASSWORD" \
-            --dbhost="$WORDPRESS_DB_HOST" \
-            --allow-root
-        
-        log "wp-config.php created"
-    fi
-}
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+  echo "✨ First run: preparing WordPress files…"
 
-main() {
-    log "Starting WordPress container initialization"
-    
-    # Wait for database
-    wait_for_mysql
-    
-    # Setup WordPress
-    setup_wordpress
-    
-    log "WordPress initialization complete"
-    log "Starting PHP-FPM"
-    
-    # Execute the main command
-    exec "$@"
-}
+  # If directory is empty (no core), fetch latest WP
+  if [ ! -f "/var/www/html/wp-load.php" ]; then
+    echo "↓ Downloading WordPress core…"
+    rm -rf /var/www/html/* /var/www/html/.[!.]* || true
+    wp core download --path=/var/www/html --allow-root
+  fi
 
-main "$@"
+  # Create wp-config.php using env vars
+  echo "⚙️ Generating wp-config.php…"
+  wp config create \
+    --path=/var/www/html \
+    --dbname="${DB_NAME}" \
+    --dbuser="${DB_USER}" \
+    --dbpass="${DB_PASSWORD}" \
+    --dbhost="${DB_HOST}:${DB_PORT}" \
+    --dbprefix="${WP_TABLE_PREFIX}" \
+    --skip-check \
+    --allow-root
+
+  # Set salts
+  echo "🔐 Injecting auth salts…"
+  wp config shuffle-salts --allow-root
+
+  # Toggle debug flags
+  if [ "${WP_DEBUG}" = "true" ] || [ "${WP_DEBUG}" = "1" ]; then
+    wp config set WP_DEBUG true --raw --type=constant --allow-root
+    wp config set WP_DEBUG_LOG true --raw --type=constant --allow-root
+    wp config set WP_DEBUG_DISPLAY false --raw --type=constant --allow-root
+  fi
+
+  # Install site if not already installed
+  if ! wp core is-installed --allow-root; then
+    echo "🧭 Running wp core install…"
+    wp core install \
+      --url="${WP_URL}" \
+      --title="${WP_TITLE}" \
+      --admin_user="${WP_ADMIN_USER}" \
+      --admin_password="${WP_ADMIN_PASSWORD}" \
+      --admin_email="${WP_ADMIN_EMAIL}" \
+      --skip-email \
+      --allow-root
+  fi
+
+  # Friendly perms for dev
+  chown -R www-data:www-data /var/www/html
+  find /var/www/html -type d -exec chmod 755 {} \;
+  find /var/www/html -type f -exec chmod 644 {} \;
+
+  echo "✅ WordPress ready at ${WP_URL}"
+else
+  echo "♻️ Using existing WordPress volume."
+fi
+
+# Ensure Apache can write logs
+mkdir -p /var/log/apache2 && chown -R www-data:www-data /var/log/apache2
+
+# Start Apache (foreground)
+exec "$@"
